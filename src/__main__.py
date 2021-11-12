9bb61ac827f6e4c279525e41a5f23aecb9a15c7c
ThaddeusTreloar <ThaddeusTreloar@github.com>
2021-11-12 14:48:17 +1100

    added util.unimplemented(), added unimplemented golbal_settings.hotSwapModule.

--- src/__main__.py
+++ src/__main__.py
@@ -1,11 +1,13 @@
 from sys import argv
-from util import panic
+from util import panic, unimplemented
 import init
 
 def main():
 
     #systemArguments = argv
     #systemArgumentsNo = len(systemArguments)
+
+    unimplemented()
 
     settings = init.init()
     settings.kernel_module.start()
